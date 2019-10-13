c6b7e04e8beff5dc2c8a387e6be1949abcf6c886
Warren Harper II <warrenharper2@att.net>
2019-10-13 19:24:00 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,6 +19,17 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
+  assembled_matrix = [ENNET_HOUSE =[
+    "Don Gately",
+    "Joelle van Dyne",
+    "Pat M.",
+    "Kate Gompert",
+    "Bruce Green"
+  ],ENFIELD_TENNIS_ACADEMY =["Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"]]
 end
 
 def array_literal_matrix
